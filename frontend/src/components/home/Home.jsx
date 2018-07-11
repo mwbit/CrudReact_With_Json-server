7b7fde915b16d2bd26dc4c8ee8c5d34283ec718f
mwbitt <mwbitt@gmail.com>
2018-07-11 11:14:02 -0300

    add crud code

---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início" subtitle="Texto do subtitulo">
+        <div className="display-4">Bem vindo!</div>
+        <hr />
+        <p className="mb-0">Example of a crud using React</p>
+    </Main>
--- frontend/src/components/template/Footer.jsx
+++ frontend/src/components/template/Footer.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Footer
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por
+            <strong>Dev <span className="text-danger">X</span>YZ</strong>
+        </span>
     </footer>
